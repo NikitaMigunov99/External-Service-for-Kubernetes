9b4ddbe24085b9c8fde73adf115675842f75ecb1
Nikita Migunov <NAMigunov@sberbank.ru>
2024-10-06 19:33:33 +0300

    Experiments with ConfigMap

--- src/main/java/com/example/external/service/Controller.java
+++ src/main/java/com/example/external/service/Controller.java
@@ -1,6 +1,7 @@
 package com.example.external.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     private final RestClient restClient;
+    @Value("${myapp.custom.value}")
+    private String value;
+    @Value("${myapp.custom.second}")
+    private String secondValue;
 
     @Autowired
     public Controller(RestClient restClient) {
@@ -19,5 +24,10 @@ public class Controller {
     public ResponseEntity<String> getFromInternal() {
         String response = restClient.getString();
         return ResponseEntity.ok("Response form internal service " + response);
+    }
+
+    @GetMapping("get")
+    public ResponseEntity<String> getSimple() {
+        return ResponseEntity.ok("Value is " + value + " second value is " + secondValue);
     }
 }
--- src/main/java/com/example/external/service/ExternalServiceApplication.java
+++ src/main/java/com/example/external/service/ExternalServiceApplication.java
@@ -2,10 +2,12 @@ package com.example.external.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableConfigurationProperties
 public class ExternalServiceApplication {
 
     public static void main(String[] args) {
